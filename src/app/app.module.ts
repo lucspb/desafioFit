084756a9fc075babab130bd8b8f27abdc97260b6
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-10-27 15:44:10 -0300

    feat: adicionando controle do formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +13,9 @@ import { FormsComponent } from './components/forms/forms.component';
     FormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
