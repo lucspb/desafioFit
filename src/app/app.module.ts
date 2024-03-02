d27a695b5accb4d31a63249d263f84196d7f15b6
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-03-02 11:28:31 -0300

    feat: create service getUnit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FormsComponent } from './components/forms/forms.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,7 @@
+import { UnitsResponse } from './../../types/units-response.interface';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { GetUnitsService } from 'src/app/services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -11,13 +13,15 @@ export class FormsComponent implements OnInit {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder,
+    private unitService: GetUnitsService) {}
 
   ngOnInit(): void {
-      this.formGroup = this.formBuilder.group({
-        hour: '',
-        showClosed: false
-      });
+    this.unitService.getAllUnits().subscribe(data => console.log(data));
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    });
   }
 
   onSubmit(): void{
