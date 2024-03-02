bdce09ebfa1abff5dc6271e07eb1f5918a93e9f8
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-03-02 17:24:13 -0300

    feat: refatorando o getUnitService e o component de forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormsComponent
+    FormsComponent,
+    CardsListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -26,14 +26,15 @@ export class FormsComponent implements OnInit {
       showClosed: true
     });
     this.unitService.getAllUnits().subscribe(data => {
-      this.results = data.locations;
-      this.filteredResults = data.locations;
+      this.results = data;
+      this.filteredResults = data;
     });
   }
 
   onSubmit(): void{
-    let {showClosed, hour} = this.formGroup.value
-    this.filteredResults = this.filterService.filter(this.results, showClosed, hour)
+    let {showClosed, hour} = this.formGroup.value;
+    this.filteredResults = this.filterService.filter(this.results, showClosed, hour);
+    this.unitService.setFilteredUnits(this.filteredResults);
   }
 
   onClean(): void{
